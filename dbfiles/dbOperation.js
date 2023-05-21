39d14f01b6f4e089112c0438d034b6b0279a1324
shushnikz <sushmithadevadiga997@gmail.com>
2023-05-21 15:27:17 +0530

    added console

--- dbfiles/dbOperation.js
+++ dbfiles/dbOperation.js
@@ -3,9 +3,13 @@ const sql = require("mssql");
 
 const getEmployees = async () => {
     try {
+        console.log("Reached1")
         let pool = await sql.connect(config);
+        console.log("Reached2")
         let employees = await pool.request().query("SELECT * from Employee")
+        console.log("Reached 3")
         console.log(employees);
+        console.log("Reached 4")
         return employees.recordset
     } catch (error) {
         console.log(error)
